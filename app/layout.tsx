cdcbcec3c1aba37fd1b7b57bf84b6d9d4eb1b845
Boahen Humphrey Agyenim-Boateng <112774674+Hboahen42@users.noreply.github.com>
2026-02-28 21:09:16 -0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const dmSans =  DM_Sans({
+    variable: "--font-dm-sans",
+    subsets: ["latin",]
 });
 
 export const metadata: Metadata = {
@@ -23,12 +18,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className="dark">
+        <head>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap"
+                rel="stylesheet"
+            />
+        </head>
+        <body
+            className={`${dmSans.variable} antialiased`}
+        >
+            {children}
+        </body>
     </html>
   );
 }
